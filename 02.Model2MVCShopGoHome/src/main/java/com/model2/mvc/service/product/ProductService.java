52768aa8bbbce661f9d8948d66e820f67f377668
user <user@DESKTOP-1MK0N1L>
2020-05-21 17:00:59 +0900

    Create 05 21

--- 02.Model2MVCShopGoHome/src/main/java/com/model2/mvc/service/product/ProductService.java
+++ 02.Model2MVCShopGoHome/src/main/java/com/model2/mvc/service/product/ProductService.java
@@ -7,13 +7,16 @@ import com.model2.mvc.service.domain.Product;
 
 
 public interface ProductService {
-	
-	public void addProduct(Product productVO) throws Exception;
-
+	//상품 추가
+	public void addProduct(Product product) throws Exception;
+	//상품 정보확인
 	public Product getProduct(int prodNo) throws Exception;
-
+	//상품리스트
 	public Map<String,Object> getProductList(Search search) throws Exception;
-
-	public void updateProduct(Product productVO) throws Exception;
+	//상품정보수정
+	public void updateProduct(Product product) throws Exception;
+	
+	public void insertProduct(Product product) throws Exception;
+	
 	
 }
